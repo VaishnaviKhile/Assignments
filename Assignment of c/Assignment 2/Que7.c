94f1ade4467808ddd6a8690b8f468a178af497ad
Vaishnavi <vaishnavikhile@gmail.com>
2022-10-31 21:32:47 +0530

    Assignment completed

--- Assignment of c/Assignment 2/Que7.c
+++ Assignment of c/Assignment 2/Que7.c
@@ -1,7 +1,9 @@
 //7. Write a program to check whether the given number is even or odd using a bitwise operator.
 #include<stdio.h>
 int main(){
-
-
+    int a;
+    printf("Enter any no. to check whether it's even or odd:");
+    scanf("%d",&a);
+    (a&1)==0?printf("Even no."):printf("Odd no.");
     return 0;
 }
